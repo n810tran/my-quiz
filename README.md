163ed5f90c105a42b658b649aed30d09468fda99
n810tran <nathaniel.k.tran@gmail.com>
2023-08-29 23:19:03 -0700

    updated README, added quiz functionality to script, added high schore section to html

---
+++ README.md
@@ -0,0 +1,21 @@
+## Quiz
+
+# Description
+
+The goal of this web application was to create a quiz. The quiz ends when the user has answered all the questions or the timer has run out. THis project incorporated many differnt aspects of JavaScript.
+
+# Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+
+# Links
+
+[Repository](https://github.com/n810tran/my-quiz)
+
+[Deployed Site](https://n810tran.github.io/my-quiz/)
+
+# Preview
+
+![Screenshot](./assets/images/screenshot.png)
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const startBtn = document.querySelector('#startBtn');
 const startScreen = document.querySelector('#startScreen');
 const quizScreen = document.querySelector('#quizScreen');
-
+const time = document.querySelector('#time')
+const quizQuestion = document.querySelector('#quizQuestion')
+const quizChoices = document.querySelector('#quizChoices')
+var count = 60
+var currentIndex = 0
 
 // Questions as an array or an object
 let questions = [
@@ -38,28 +42,57 @@ let questions = [
 ]
 
 // Code a start button that starts a timer and begins the quiz with a question
-startBtn.addEventListener('click', startGame())
+startBtn.addEventListener("click", startGame)
 
-startGame() {
+function startGame() {
+    time.textContent = --count + " seconds left" // Displays timer instantly
     startScreen.setAttribute('class', 'hide');
     quizScreen.setAttribute('class','show');
-    timer = setInterval(function(){
-        
-    })
+    let timer = setInterval(function(){
+        time.textContent = --count + " seconds left";
+        if(count <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000)
+    displayQuestion()
 }
 
 // When a question is answered, present next question
 function displayQuestion(){
+    let currentQuestion = questions[currentIndex];
+    quizQuestion.textContent = currentQuestion.question;
+    quizChoices.innerHTML = "";
 
-
+    for (let i=0;i<questions[currentIndex].choices.length;i++) {
+        const choiceBtn = document.createElement('button');
+        choiceBtn.textContent = questions[currentIndex].choices[i];
+        choiceBtn.addEventListener('click', checkAnswer);
+        quizChoices.appendChild(choiceBtn);
+    }
 }
 
 // When a question is answered incorrectly, subtract time from clock
+function checkAnswer(event) {
+    const selectedAnswer = event.target.textContent;
+    const correctAnswer = questions[currentIndex].correct;
+    if(selectedAnswer !== correctAnswer) {
+        count -= 10;
+    } else {
+        currentIndex++;
+    if (currentIndex === questions.length) {
+        endGame();
+    } else {
+        displayQuestion();
+    }
+    }
 
+}
 
 // When all questions are answered or when timer reaches 0, end game
 function endGame(){
-
+    clearInterval(timer);
+    
 
 }
 // When game is over, save initials and score
